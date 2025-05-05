c2623f9fd02d86990f79492001ff4867deed180f
ShehanPremathilaka2000 <harshanashehan52@gmail.com>
2025-05-06 01:56:26 +0530

    Update TFIDFTest to use 'words' as input column and add NaiveBayesPipelineTest for pipeline verification - pass all the test cases

--- spark-driver/src/test/java/com/lohika/morning/ml/spark/driver/service/lyrics/pipeline/NaiveBayesPipelineTest.java
+++ spark-driver/src/test/java/com/lohika/morning/ml/spark/driver/service/lyrics/pipeline/NaiveBayesPipelineTest.java
@@ -2,7 +2,10 @@ package com.lohika.morning.ml.spark.driver.service.lyrics.pipeline;
 
 import com.lohika.morning.ml.spark.driver.service.BaseTest;
 import com.lohika.morning.ml.spark.driver.service.MLService;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.assertNotNull;
 
 public class NaiveBayesPipelineTest extends BaseTest {
 
@@ -11,7 +14,12 @@ public class NaiveBayesPipelineTest extends BaseTest {
 
     @Autowired
     private MLService mlService;
-
-
+    
+    @Test
+    public void testNaiveBayesPipelineExists() {
+        // Simple test to verify the pipeline is autowired correctly
+        assertNotNull("NaiveBayesBagOfWordsPipeline should be autowired", naiveBayesPipeline);
+        assertNotNull("MLService should be autowired", mlService);
+    }
 
 }
